0424928899c54f3e21c33ba9032a529b3d121c4c
Mehedi0101 <mehedi29121998@gmail.com>
2023-12-20 23:58:14 +0600

    content and styles of banner updated

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -7,9 +7,9 @@ const Banner = () => {
             <div className="hero-overlay bg-opacity-60 bg-[#1C1C1C]"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-lg">
-                    <motion.h5 animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0, repeat: Infinity, repeatDelay: 3 }} className='lg:text-xs text-[10px] tracking-widest font-light mb-2'>TASTY AND CRUNCHY</motion.h5>
-                    <motion.h1 animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.2, repeat: Infinity, repeatDelay: 3 }} className="mb-5 xl:text-6xl lg:text-5xl md:text-4xl text-3xl font-bold">Savor Culinary Excellence</motion.h1>
-                    <motion.p animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.4, repeat: Infinity, repeatDelay: 3 }} className="mb-5 lg:text-base text-sm font-medium">Embark on a gastronomic journey that transcends the ordinary. Our expert chefs blend passion and innovation, creating culinary masterpieces that delight the senses.</motion.p>
+                    <motion.h5 animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0, repeat: Infinity, repeatDelay: 3 }} className='lg:text-xs text-[10px] tracking-widest font-light mb-2'>SAIL SMOOTHLY THROUGH TASKS</motion.h5>
+                    <motion.h1 animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.2, repeat: Infinity, repeatDelay: 3 }} className="mb-5 lg:text-5xl md:text-4xl text-3xl font-bold">Welcome to WorkWave!</motion.h1>
+                    <motion.p animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.4, repeat: Infinity, repeatDelay: 3 }} className="mb-5 lg:text-base text-sm">Streamline tasks, boost collaboration, and conquer deadlines effortlessly with our intuitive task management platform. Elevate your productivity journey now!</motion.p>
                     <motion.button className="px-5 py-2 font-semibold text-sm bg-primary text-white active:scale-95 transition-all">VIEW MENU</motion.button>
                 </div>
             </div>
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
+import OurTeam from "../pages/OurTeam";
+import SupportUs from "../pages/SupportUs";
 
 const routes = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/our-team',
+                element: <OurTeam></OurTeam>
+            },
+            {
+                path: '/support-us',
+                element: <SupportUs></SupportUs>
             }
         ]
     }
