1931d0519784324e22ae125dd5fa795fc6ecc26f
Mehedi0101 <mehedi29121998@gmail.com>
2023-12-21 08:46:04 +0600

    banner and navbar slightly modified

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -9,8 +9,8 @@ const Banner = () => {
                 <div className="max-w-lg">
                     <motion.h5 animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0, repeat: Infinity, repeatDelay: 3 }} className='lg:text-xs text-[10px] tracking-widest font-light mb-2'>SAIL SMOOTHLY THROUGH TASKS</motion.h5>
                     <motion.h1 animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.2, repeat: Infinity, repeatDelay: 3 }} className="mb-5 lg:text-5xl md:text-4xl text-3xl font-bold">Welcome to WorkWave!</motion.h1>
-                    <motion.p animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.4, repeat: Infinity, repeatDelay: 3 }} className="mb-5 lg:text-base text-sm">Streamline tasks, boost collaboration, and conquer deadlines effortlessly with our intuitive task management platform. Elevate your productivity journey now!</motion.p>
-                    <motion.button className="px-5 py-2 font-semibold text-sm bg-primary text-white active:scale-95 transition-all">VIEW MENU</motion.button>
+                    <motion.p animate={{ y: [-50, 0], opacity: [0, 1] }} transition={{ duration: 0.5, delay: 0.4, repeat: Infinity, repeatDelay: 3 }} className="mb-5 lg:text-base text-sm text-lightDark">Streamline tasks, boost collaboration, and conquer deadlines effortlessly with our intuitive task management platform. Elevate your productivity journey now!</motion.p>
+                    <motion.button className="px-5 py-3 font-semibold md:text-sm text-xs bg-primary text-white active:scale-95 transition-all">LET&apos;S EXPLORE</motion.button>
                 </div>
             </div>
         </div>
---
+++ src/components/home/ForWhom.jsx
@@ -0,0 +1,9 @@
+const ForWhom = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ForWhom;
