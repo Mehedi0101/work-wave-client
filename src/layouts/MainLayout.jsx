47a873049330d9fdbcee14e404b86297464e76a8
Mehedi0101 <mehedi29121998@gmail.com>
2023-12-21 09:58:03 +0600

    footer added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
     return (
@@ -8,6 +9,7 @@ const MainLayout = () => {
             <div className="min-h-[60vh]">
                 <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
