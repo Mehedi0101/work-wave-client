24e762ff2edaf7b9cedeab126970da87fd16a2c6
Mehedi0101 <mehedi29121998@gmail.com>
2023-12-22 15:23:01 +0600

    contents added in support us page

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -4,21 +4,24 @@ import teammate3 from "../assets/member3.jpg";
 
 const OurTeam = () => {
     return (
-        <div className='grid items-center grid-cols-1 md:grid-cols-3 gap-10 mb-14 text-center font-primary pt-36 md:px-10 px-5 select-none max-w-screen-2xl mx-auto'>
-            <div className="">
-                <img className="w-full" src={teammate1} alt="" />
-                <h3 className="tracking-widest text-sm text-dark2 mt-5">Project Manager</h3>
-                <h2 className="lg:text-3xl md:text-2xl text-xl">Nathan Cole</h2>
-            </div>
-            <div className="">
-                <img className="w-full" src={teammate3} alt="" />
-                <h3 className="tracking-widest text-sm text-dark2 mt-5">Team Lead</h3>
-                <h2 className="lg:text-3xl md:text-2xl text-xl">Liam Parker</h2>
-            </div>
-            <div className="">
-                <img className="w-full" src={teammate2} alt="" />
-                <h3 className="tracking-widest text-sm text-dark2 mt-5">Developer</h3>
-                <h2 className="lg:text-3xl md:text-2xl text-xl">Mason Hayes</h2>
+        <div className="mb-14 text-center font-primary lg:pt-36 pt-20 md:px-10 px-5 select-none max-w-screen-2xl mx-auto">
+            <h2 className={`md:text-4xl text-3xl font-bold text-center mb-10 text-black pt-10`}>Our Team</h2>
+            <div className='grid items-center grid-cols-1 md:grid-cols-3 gap-10'>
+                <div className="">
+                    <img className="w-full" src={teammate1} alt="" />
+                    <h3 className="tracking-widest text-sm text-dark2 mt-5">Project Manager</h3>
+                    <h2 className="lg:text-3xl md:text-2xl text-xl">Nathan Cole</h2>
+                </div>
+                <div className="">
+                    <img className="w-full" src={teammate3} alt="" />
+                    <h3 className="tracking-widest text-sm text-dark2 mt-5">Team Lead</h3>
+                    <h2 className="lg:text-3xl md:text-2xl text-xl">Liam Parker</h2>
+                </div>
+                <div className="">
+                    <img className="w-full" src={teammate2} alt="" />
+                    <h3 className="tracking-widest text-sm text-dark2 mt-5">Developer</h3>
+                    <h2 className="lg:text-3xl md:text-2xl text-xl">Mason Hayes</h2>
+                </div>
             </div>
         </div>
     );
--- src/pages/SupportUs.jsx
+++ src/pages/SupportUs.jsx
@@ -1,7 +1,9 @@
 const SupportUs = () => {
     return (
-        <div>
-            
+        <div className="mb-14 text-center font-primary lg:pt-36 pt-20 md:px-10 px-5 select-none max-w-screen-2xl mx-auto">
+            <h2 className={`md:text-4xl text-3xl font-bold text-center mb-5 text-black pt-10`}>Support Us</h2>
+            <p className="max-w-md mx-auto mb-8 md:text-base text-sm">Keeping WORKWAVE free requires resources. If you appreciate the value our project brings to you or your team, consider making a donation. Every contribution, no matter the size, helps us maintain and enhance our services.</p>
+            <button className="px-5 py-3 font-semibold md:text-sm text-xs bg-primary text-white active:scale-95 transition-all">DONATE</button>
         </div>
     );
 };
